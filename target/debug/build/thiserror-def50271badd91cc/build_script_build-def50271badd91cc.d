e316274d9a6cd3527646c8cb2a1630cf348893fe
Oleksii Molchanov <eyexzy@gmail.com>
2024-11-20 23:42:29 +0200

    First commit with cleaned project structure

--- target/debug/build/thiserror-def50271badd91cc/build_script_build-def50271badd91cc.d
+++
@@ -1,5 +0,0 @@
-C:\Users\pc\calculator\target\debug\build\thiserror-def50271badd91cc\build_script_build-def50271badd91cc.exe: C:\Users\pc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.69\build.rs
-
-C:\Users\pc\calculator\target\debug\build\thiserror-def50271badd91cc\build_script_build-def50271badd91cc.d: C:\Users\pc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.69\build.rs
-
-C:\Users\pc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.69\build.rs:
--- target/debug/calculator.d
+++
@@ -1,1 +0,0 @@
-C:\Users\pc\calculator\target\debug\calculator.exe: C:\Users\pc\calculator\src\calculator.pest C:\Users\pc\calculator\src\main.rs
